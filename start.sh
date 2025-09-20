3934dea02e6418323c8f887fd3993bbffb5da9c4
kumart2prox <itskumar.9.x@gmail.com>
2025-09-20 08:57:13 +0530

    Update start.sh 480p

--- start.sh
+++ start.sh
@@ -7,11 +7,11 @@ while true
 do
     ffmpeg -re \
       -stream_loop -1 -i mainvideonosound.mp4 \
-      -stream_loop -1 -i song1.mp3 \
-      -c:v libx264 -b:v 3000k -maxrate 3000k -bufsize 6000k -s 1280x720 -preset veryfast \
-      -c:a aac -b:a 128k -pix_fmt yuv420p \
-      -g 60 -keyint_min 48 \
-      -force_key_frames "expr:gte(t,n_forced*4)" \
+      -stream_loop -1 -i song2.mp3 \
+      -c:v libx264 -b:v 1000k -maxrate 1000k -bufsize 2000k -s 854x480 -preset veryfast \
+      -c:a aac -b:a 96k -ar 44100 -pix_fmt yuv420p \
+      -g 30 -keyint_min 30 \
+      -r 30 -force_key_frames "expr:gte(t,n_forced*2)" \
       -shortest \
       -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
 done
